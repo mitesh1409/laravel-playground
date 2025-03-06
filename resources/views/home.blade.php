05ba4d75077b9fe1e4d88a80d2aaf194aebc99a3
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-03-06 23:11:30 +0530

    Add a layout component and make use of it

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel | Home</title>
-</head>
-<body>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </nav>
-
-    <h1>Welcome Home!</h1>
-</body>
-</html>
+<x-layout>
+    <h1>Welcome Home :)</h1>
+</x-layout>
